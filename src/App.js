5c119de1ba4ecc5faa6d676b63e476cf57b790a4
Myeongkook Park <myeongkuk@hotmail.com>
2021-07-17 00:40:48 +0900

    7/17 Toggle 기능

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 function App() {
-  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '첫 번째 리액트 프로젝트',
+      checked: true,
+    },
+  ]);
+  const nextId = useRef(2);
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id == id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
 }
 
 export default App;
